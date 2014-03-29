8145f745261c38e85dce1b94b10f7361c6dbdebe
Lawrence Wang <l@lw.io>
2014-03-29 14:45:49 -0700

    setup testing for server failure

--- striper.rb
+++ striper.rb
@@ -6,6 +6,10 @@ Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
 get '/pay' do
   amount, token, email, callback = params.values_at(:amount, :token, :email, :cb)
   begin
+    if email == 'test@fail.com'
+      raise 'something is on fire'
+    end
+
     charge = Stripe::Charge.create(
       card:        token,
       amount:      (amount.to_i * 100).round,
