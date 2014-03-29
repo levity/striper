63a2a4ab53273cf64548690de002175a4e0d3fa5
Lawrence Wang <l@lw.io>
2014-03-29 14:28:47 -0700

    actually charge stripe

--- striper.rb
+++ striper.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
+require 'stripe'
+
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
 
 get '/pay' do
-  callback = request['cb']
-  result = request.query_string
-  "#{callback}('#{result}');"
+  amount, token, email, callback = params.values_at(:amount, :token, :email, :cb)
+  begin
+    charge = Stripe::Charge.create(
+      card:        token,
+      amount:      (amount.to_i * 100).round,
+      currency:    'usd',
+      description: email
+    )
+    result = {}
+  rescue
+    result = {error: $!.message}
+  end
+  "#{callback}(#{result.to_json});"
 end
 
